cb46ff26c849db74a4713fd0d9b6ecfc12ec6c79
Mercy <mercymumbe17@gmail.com>
2025-03-21 01:38:58 +0300

    modified DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.getElementById("main");
+if (main){
+    main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent =  "Mercy is the champion" 
+
